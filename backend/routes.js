d3302ad01851bc723eb8cfee0bdc81fb7847cd2e
Jakob Kohler <schasch.dev@protonmail.com>
2025-02-12 19:09:24 +0100

    Added SQLite DB and some server code

---
+++ backend/routes.js
@@ -0,0 +1,7 @@
+const endpoints = {
+    '/api/v1': 'API root',
+    '/api/v1/pizzaStats': 'Get pizza data of all users',
+    '/api/v1/pizzaStats/:user': 'Get pizza data for a specific user'
+};
+
+module.exports = endpoints;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const endpoints = require('./routes.js')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+
+app.get('/api/v1', (req, res) => {
+    res.json({
+        apiVersion: '1.0',
+        endpoints: endpoints,
+    });
+});
+
+app.get("api/v1/pizzaStats", (req, res) => {
+    
 })
 
 app.listen(port, () => {
