c7e2307ad5493b9d14728173977bd7953c2a0da0
Jakob Kohler <schasch.dev@protonmail.com>
2025-02-14 02:00:24 +0100

    added db init

---
+++ backend/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Running createUsers.js..."
+node createUsers.js mani schasch master
+
+if [ $? -eq 0 ]; then
+  echo "Database initialized successfully. Starting the server..."
+  node server.js
+else
+  echo "Error: Failed to initialize the database."
+  exit 1
+fi
